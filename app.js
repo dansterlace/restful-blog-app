d948c8e0555ca8baa008714b88587c7f72a7daf8
Dan <dpsterlace@gmail.com>
2017-01-20 04:00:44 +0000

    last bit of touch ups

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var methodOverride  =   require("method-override"),
-    bodyParser      =   require("body-parser"),
-    mongoose        =   require("mongoose"),
-    express         =   require("express"),
-    app             =   express();
+var expressSanitizer    =   require("express-sanitizer"),
+    methodOverride      =   require("method-override"),
+    bodyParser          =   require("body-parser"),
+    mongoose            =   require("mongoose"),
+    express             =   require("express"),
+    app                 =   express();
     
     
 // APP CONFIG
@@ -11,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 
 // MONGOOSE/ MODEL CONFIG
@@ -50,6 +52,7 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -84,6 +87,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
